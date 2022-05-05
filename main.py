b040c90fa7056d9acddd949edf500ba40d2b61df
xyoouy <egorloparev@gmail.com>
2022-05-05 19:16:20 +0500

    Add "BackGround"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from ctypes import windll
+import random
 
 
 windll.shcore.SetProcessDpiAwareness(1)
@@ -14,41 +15,41 @@ button.pack(side=BOTTOM)
 
 
 def get_text():
-    with open("text.txt", 'r') as f:
+    number = random.randrange(1, 5, 1)
+    with open(str(number)+".txt", 'r', encoding="utf-8") as f:
         return f.read()
 
 
 def key_pressed(event):
-    global text_content, cur_index, text
+    global text_content, cur_index, main_text
     if event.char == "":
         return
-    text.config(state=NORMAL)
+    main_text.config(state=NORMAL)
     if event.char == text_content[cur_index]:
         cur_index += 1
-        text.tag_add("current", f"1.{cur_index}", f"1.{cur_index+1}")
-        text.tag_add("previous", f"1.0", f"1.{cur_index}")
+        main_text.tag_add("current", f"1.{cur_index}", f"1.{cur_index+1}")
+        main_text.tag_add("previous", f"1.0", f"1.{cur_index}")
     else:
-        text.tag_add("wrong", f"1.{cur_index}", f"1.{cur_index+1}")
+        main_text.tag_add("wrong", f"1.{cur_index}", f"1.{cur_index+1}")
 
-    text.config(state=DISABLED)
+    main_text.config(state=DISABLED)
 
 
 cur_index = 0
 text_content = get_text()
 
-
-text = Text(
+main_text = Text(
     root,
     font=("Consolas", 14)
 )
-text.insert(INSERT, text_content)
-text.tag_config("current", background="green", foreground="white")
-text.tag_config("previous", background="white", foreground="green")
-text.tag_config("wrong", foreground="red")
+main_text.insert(INSERT, text_content)
+main_text.tag_config("current", background="green", foreground="white")
+main_text.tag_config("previous", background="white", foreground="green")
+main_text.tag_config("wrong", foreground="red")
 
-text.tag_add("current", f"1.{cur_index}", f"1.{cur_index+1}")
-text.config(state=DISABLED)
-text.pack(ipadx=10, ipady=10)
+main_text.tag_add("current", f"1.{cur_index}", f"1.{cur_index+1}")
+main_text.config(state=DISABLED)
+main_text.pack(ipadx=10, ipady=10)
 
 root.bind("<Key>", key_pressed)
 
