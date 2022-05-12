657238aba6c92290fa2b1f5d55e00df1d584846c
xyoouy <egorloparev@gmail.com>
2022-05-12 13:43:50 +0500

    Typing 2.1

--- main.py
+++ main.py
@@ -10,14 +10,39 @@ root.attributes('-fullscreen', True)
 root["bg"] = "#54c6ff"
 root.title('Tkinter Window Demo')
 
-button = Button(text="Выключить", width=15, height=5, bg="grey", fg="white", command=root.destroy)
-button.pack(side=BOTTOM)
+
+def add_button(text, width, height, bg, fg, command, side):
+    button = Button(text=text, width=width, height=height, bg=bg, fg=fg, command=command)
+    button.pack(side=side)
+    return button
 
 
 def get_text():
     number = random.randrange(1, 9)
     with open("Texts/" + str(number) + ".txt", 'r', encoding="utf-8") as f:
         return f.read()
+
+
+def create_text():
+    main_text = Text(root,
+                     font=("Consolas", 14))
+
+    main_text.insert(INSERT, text_content)
+    main_text.tag_config("current", background="green", foreground="white")
+    main_text.tag_config("previous", background="white", foreground="green")
+    main_text.tag_config("wrong", foreground="red")
+    main_text.config(state=DISABLED)
+    main_text.pack(ipadx=10, ipady=10)
+
+    return main_text
+
+
+def get_accuracy_text_template(percentage):
+    return f"Точность:\n {percentage}%"
+
+
+def get_speed_rate_text_template(rate):
+    return f"Скорость:\n {rate} зн./мин"
 
 
 def calculate_correctness_percentage():
@@ -27,9 +52,13 @@ def calculate_correctness_percentage():
     return (cur_index - mistook_times) / cur_index * 100
 
 
+def add_tag(name, first, second):
+    global main_text
+    main_text.tag_add(name, f"1.{first}", f"1.{second}")
+
+
 def key_pressed(event):
-    global text_len, text_content, cur_index, main_text, mistook_times, correctness_percentage,\
-        mistook_letter, percentage_label, char_per_minutes, start_time
+    global cur_index, mistook_times, mistook_letter, start_time
     if cur_index == 0:
         start_time = time.time()
     if event.char == "":
@@ -38,8 +67,8 @@ def key_pressed(event):
     if event.char == text_content[cur_index]:
         mistook_letter = False
         cur_index += 1
-        main_text.tag_add("current", f"1.{cur_index}", f"1.{cur_index + 1}")
-        main_text.tag_add("previous", f"1.0", f"1.{cur_index}")
+        add_tag("current", cur_index, cur_index + 1)
+        add_tag("previous", cur_index - 1, cur_index)
         if cur_index != 0:
             char_per_minutes = int((cur_index / (time.time() - start_time)) * 60)
             speed_rate_label.config(text=get_speed_rate_text_template(char_per_minutes))
@@ -47,7 +76,7 @@ def key_pressed(event):
         if not mistook_letter:
             mistook_times += 1
             mistook_letter = True
-        main_text.tag_add("wrong", f"1.{cur_index}", f"1.{cur_index + 1}")
+        add_tag("wrong", cur_index, cur_index + 1)
 
     main_text.config(state=DISABLED)
 
@@ -55,49 +84,32 @@ def key_pressed(event):
     percentage_label.config(text=get_accuracy_text_template(correctness_percentage))
 
 
-def get_accuracy_text_template(percentage):
-    return f"Точность:\n {percentage}%"
+def create_statistic(text, value, side):
+    label = Label(text=text(value),
+                  width=10,
+                  height=10, )
+    label.pack(side=side)
+    return label
 
-def get_speed_rate_text_template(rate):
-    return f"Скорость:\n {rate} зн./мин"
 
+button = add_button(text="Выключить", width=15, height=5, bg="grey", fg="white", command=root.destroy, side=BOTTOM)
 
 cur_index = 0
 text_content = get_text()
 text_len = len(text_content)
+char_per_minutes = 0
 
 mistook_times = 0
 correctness_percentage = 100
 mistook_letter = False
 
-percentage_label = Label(
-    text=get_accuracy_text_template(correctness_percentage),
-    width=10,
-    height=10,
-)
-percentage_label.pack(side=LEFT)
+percentage_label = create_statistic(get_accuracy_text_template, correctness_percentage, side=LEFT)
 
-main_text = Text(
-    root,
-    font=("Consolas", 14)
-)
+speed_rate_label = create_statistic(get_speed_rate_text_template, char_per_minutes, side=RIGHT)
 
-char_per_minutes = 0
+main_text = create_text()
 
-speed_rate_label = Label(text=get_speed_rate_text_template(char_per_minutes),
-                         width=10,
-                         height=10)
-
-speed_rate_label.pack(side=RIGHT)
-
-main_text.insert(INSERT, text_content)
-main_text.tag_config("current", background="green", foreground="white")
-main_text.tag_config("previous", background="white", foreground="green")
-main_text.tag_config("wrong", foreground="red")
-
-main_text.tag_add("current", f"1.{cur_index}", f"1.{cur_index + 1}")
-main_text.config(state=DISABLED)
-main_text.pack(ipadx=10, ipady=10)
+add_tag("current", cur_index, cur_index + 1)
 
 root.bind("<Key>", key_pressed)
 
