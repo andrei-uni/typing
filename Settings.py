20d170a5a56da56cc8b03fe46702065b06198be9
xyoouy <egorloparev@gmail.com>
2022-05-26 12:01:00 +0500

    Add save settings button

--- Settings.py
+++ Settings.py
@@ -4,6 +4,7 @@ from tkinter import *
 class Settings(Tk):
     def __init__(self, language, main_class):
         super().__init__()
+        self.save_btn = None
         self.focus_set()
         self.attributes("-topmost", True)
 
@@ -32,22 +33,32 @@ class Settings(Tk):
         )
 
         option_menu.grid(column=1, row=0, sticky=W, **paddings)
+        self.save_btn = Button(self, text="Сохранить", command=self.save_closing)
+        self.save_btn.grid(column=3, padx=120, pady=70)
 
     def setup_master(self):
         self.title("Настройки")
-        self.geometry("600x200")
+        self.geometry("450x200")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def update_language_label(self):
         self.label.pack(side=BOTTOM)
         self.label.config(text=self.language_selected.get())
 
-    def on_closing(self):
-        if self.last_language != self.language_selected.get():
-            self.main.restart()
+    def save_closing(self):
+        self.on_closing(save=True)
+
+    def on_closing(self, save=False):
+        if save:
+            if self.last_language != self.language_selected.get():
+                self.main.restart()
+            else:
+                self.withdraw()
+                self.update_language_label()
+                self.setting_on = False
         else:
+            self.language_selected.set(self.last_language)
             self.withdraw()
-            self.update_language_label()
             self.setting_on = False
 
     def run(self):
